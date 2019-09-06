7b0b17e99e5d6b0128087a065003ea17cee551d0
lvlaxjh <2876363140@qq.com>
2019-09-06 10:40:52 +0800

    111

---
+++ BUPTSEARCH_FINAL/src/java/UserSetting.java
@@ -0,0 +1,8 @@
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "userSetting")
+@SessionScoped
+public class UserSetting implements Serializable {}
